3f7f5779645abb127d57b4aa020ded161432a591
devopscloudtrainer <119645417+devopscloudtrainer@users.noreply.github.com>
2025-07-18 09:52:09 +0530

    Create automation.sh

---
+++ automation.sh
@@ -0,0 +1,14 @@
+GIT - Global Interaction Tool
+
+Git is an source code management tool which is used for storing all the code , important files, directories, images etc which uses distributed version control system mechanism which provides proper tracking and also benefit of storage cost optimization.
+
+=============================================================================================================================
+Why Git? 
+=============================================================================================================================
+* What is VCS? Version control system?
+--------------------------------------------------
+VCS - Version control system is an mechanism which provides an unique identification for managing the data inside the files via commit_id which is an hexa decimal code  in the form of 125/256 bytes using algorithm called "SHA key" - Secure hashing algorithm.
+
+eg: commit_id - nxni9bd200dh2idhihdnhnbd21dn21
+
+   Every changes will be recorded via commit_id to track the changes and avoid duplication of the data.

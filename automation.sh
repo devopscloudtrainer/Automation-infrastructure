a2d221886ad78cdf3510619dead7fae6d6626a6c
GoudSagar <goud.sagat.t@gmail.com>
2025-07-18 04:25:25 +0000

    RTicket-9882 Upating automation script for web page

--- automation.sh
+++ automation.sh
@@ -12,3 +12,20 @@ VCS - Version control system is an mechanism which provides an unique identifica
 eg: commit_id - nxni9bd200dh2idhihdnhnbd21dn21
 
    Every changes will be recorded via commit_id to track the changes and avoid duplication of the data.
+------------------------------------------------------
+* Why VCS?
+------------------------------------------------------
+1. Tracking: Who/When/What modified - Proper tracking.
+              * Every modification - capture(email and username)
+              * Proper date and time - timestamp
+              * What modified - added/deleted/modified - each and evert line/word/sentence/files/directories.
+
+2. Storage cost optimization: Avoid the duplication of the data via VCS mechanism
+
+Ecommerce-website:
+  * login-feature
+  * logout-feature
+  * Add-to-cart
+  * Payment method
+  * Product catalog
+  * Acknowledgment
